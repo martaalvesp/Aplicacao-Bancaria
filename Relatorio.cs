8e3b91113750e130118d6af2061b66d309adf293
MARTA ALVES PINHO <160552842+martaalvesp@users.noreply.github.com>
2025-11-04 18:08:33 -0300

    Add files via upload

---
+++ Relatorio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AplicacaoBancaria
+{
+    public class Relatorio
+    {
+        public void GerarExtrato(ContaBancaria conta)
+        {
+            Console.WriteLine("\nEXTRATO BANCÁRIO");
+            conta.ExibirDadosConta();
+        }
+    }
+
+}
